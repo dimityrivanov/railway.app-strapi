45c4566ce44a32f9d3795db08b6eb2c45a338945
Dimitar ivanov <dimitarivanovit@gmail.com>
2025-01-31 21:41:13 +0200

    strapi update

---
+++ src/api/beach/routes/beach.ts
@@ -0,0 +1,7 @@
+/**
+ * beach router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::beach.beach');
---
+++ src/api/beach/services/beach.ts
@@ -0,0 +1,7 @@
+/**
+ * beach service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::beach.beach');
